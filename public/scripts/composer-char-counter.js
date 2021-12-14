1c2cdb216b6d268f20e78a6db93e135eac805a33
Eric Moolenbeek <emoolenbeek@gmail.com>
2021-12-14 21:53:14 +0000

    add css reset and re style pages

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+  messageCount();
+
+});
+
+const messageCount = () => {
+  const message = document.getElementById('tweet-text');
+  const counter = document.getElementById('counter');
+  message.addEventListener('input', function (element) {
+    const target = element.target;
+
+    // count the current number of characters
+    const currentLength = target.value.length;
+    counter.innerHTML = 140 - currentLength;
+
+    // change to counter to red if below 0
+    if (counter.innerHTML > -1) {
+      counter.className = "counter"; 
+    } else {
+      counter.className = "counter-red"; 
+    }
+  });
+}
